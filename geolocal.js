227cee03a47c3ecef2544f07f937779a5f4fc9c5
Renan Couto <renancouto@gmail.com>
2012-10-03 08:20:15 -0300

    Yahoo service added
    To do: Maxmind service return (ajax or script?)

--- geolocal.js
+++ geolocal.js
@@ -13,19 +13,21 @@
 			},
 
 			yahoo: {
-				url: 'http://where.yahooapis.com/geocode?q={{latitude}},{{longitude}}&flags=J&gflags=R&callback={{callback}}',
+				url: 'http://where.yahooapis.com/geocode?q={{latitude}},{{longitude}}&flags=J&gflags=R',
 				converter: true
 			}
 		},
 
 		// Lib configuration
 		config = {
+			conectionType: 'GET',
+
 			services: {
 				independent: 'maxmind',	// This service should return the data formated,
 				dependent: 'yahoo'		// but this will convert the coordinates to known places
 			},
 
-			useBrowserAPI: true,		// Will use browser's geolocation to get the user's latitude / longitude
+			useBrowserAPI: false,		// Will use browser's geolocation to get the user's latitude / longitude
 			keepData: true				// Will keep the data on user's browser using localStorage and will try to get that data whenever it's available
 		},
 
@@ -39,44 +41,69 @@
 		},
 
 	Init = function() {
-		console.log(features, features.geolocation, features.json, features.localStorage, features.XMLHttpRequest);
-		service = features.geolocation ? config.services.dependent : config.services.independent;
+		service = config.useBrowserAPI ? config.services.dependent : config.services.independent;
 
 		if (features.localStorage && localStorage.Geolocal && config.keepData) {
 			Geolocal = Data.Convert.FromString(localStorage.Geolocal);
-			console.log(Geolocal);
+			Data.Set();
 			return;
 		}
 
 		if (features.geolocation && config.useBrowserAPI) {
-			Coordinates.Get();
+			Coords.Get();
 		}
 		else {
 			Data.Load();
 		}
 	},
 
-	Coordinates = {
+	Coords = {
 		Get: function() {
 			navigator.geolocation.getCurrentPosition(function(position) {
 				Geolocal.latitude = position.coords.latitude;
 				Geolocal.longitude = position.coords.longitude;
 				Data.Load(true);
-			});
+			}, Coords.Error);
+		},
+
+		Error: function(error) {
+			console.log(error);
 		}
 	},
 
 	Data = {
 		Load: function(reverse) {
+			var request = new XMLHttpRequest(),
+				url = services[service].url;
 
+			if (config.useBrowserAPI) {
+				url = url.replace('{{latitude}}', Geolocal.latitude).replace('{{longitude}}', Geolocal.longitude);
+			}
 
-			Data.Set();
+			console.log(url);
+
+			request.open(config.conectionType, url, true);
+			request.send(null);
+
+			request.onreadystatechange = function() {
+				if (request.readyState === 4) {
+					if (request.status === 200) {
+						Services[service](request.responseText);
+						Data.Set();
+					}
+					else {
+						console.error('Geolocal ERROR: could not get any data', request);
+					}
+				}
+			};
 		},
 
 		Set: function() {
-			if (features.localStorage && config.keepData) {
+			if (features.localStorage && config.keepData && !localStorage.Geolocal) {
 				localStorage.Geolocal = Data.Convert.ToString(Geolocal);
 			}
+
+			window.Geolocal = Geolocal;
 		},
 
 		Convert: {
@@ -91,6 +118,19 @@
 					return JSON.parse(data);
 				}
 			}
+		}
+	},
+
+	Services = {
+		yahoo: function(data) {
+			Geolocal = Data.Convert.FromString(data);
+			Geolocal = Geolocal.ResultSet.Result;
+		},
+
+		maxmind: function(data) {
+			// eval("function geoip_country_code() { return 'BR'; }");
+			console.log(data);
+			// setTimeout(function(){ eval(data); console.log('ok'); }, 10);
 		}
 	};
 
